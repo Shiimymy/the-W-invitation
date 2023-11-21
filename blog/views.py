0450d76a495f7a9036c13ee54ded6b598f5e1863
Shiimymy <>
2023-11-21 22:10:34 +0000

    Add home page html template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+def index(request):
+    """ A view to return the index page """
+    return render(request, 'index.html')
