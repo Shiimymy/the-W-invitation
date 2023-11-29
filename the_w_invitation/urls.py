6b2fc4b4052372145f74e30753570107c3ef57ba
Shiimymy <>
2023-11-29 16:44:04 +0000

    Add summernote to project

--- the_w_invitation/urls.py
+++ the_w_invitation/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),
     path('accounts/', include('allauth.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]
