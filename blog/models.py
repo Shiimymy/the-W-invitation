97776d09b9f8f095597b78b7a6edf5b574d7ff19
Shiimymy <>
2023-11-29 16:40:58 +0000

    Add DB model for memory post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+
+
+class Memory(models.Model):
+    image = models.ImageField(upload_to='media/image', default='')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"{self.author} memory:{self.content}"
+    
